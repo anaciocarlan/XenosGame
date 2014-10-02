9cc0de8d842abfa0ad678eb3c9ed062126857381
thunderstruck47 <vgerov93@yahoo.com>
2014-10-02 19:27:15 +0100

    initial commit

---
+++ desktop/src/com/xenos/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.xenos.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.xenos.game.XenosGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new XenosGame(), config);
+	}
+}
